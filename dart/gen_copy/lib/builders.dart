9547606ce1732e84d5387d8eed806599e8b30d2e
hilltwice <hilltwice@163.com>
2023-11-08 21:10:47 +0800

    dart code gen basic frame

--- dart/gen_copy/lib/builders.dart
+++ dart/gen_copy/lib/builders.dart
@@ -1,1 +1,10 @@
+import 'package:build/build.dart';
+import 'package:gen_copy/src/copy.dart';
+import 'package:source_gen/source_gen.dart';
 
+Builder copyBuilder(BuilderOptions options) {
+  return LibraryBuilder(
+    CopyGenerator(),
+    generatedExtension: '.copy.g.dart',
+  );
+}
