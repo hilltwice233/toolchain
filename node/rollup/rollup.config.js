e8bd15329841eb3e6576a2ce5473aa2d7f8da9a7
hilltwice <hilltwice@163.com>
2023-11-08 23:34:18 +0800

    build bin config

--- node/rollup/rollup.config.js
+++ node/rollup/rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from "@rollup/plugin-commonjs"
 import nodeResolve from "@rollup/plugin-node-resolve"
 import terser from "@rollup/plugin-terser"
 import typescript from "@rollup/plugin-typescript"
+import {defineConfig} from "rollup"
 
 export const plugins = [
   typescript(),
@@ -9,3 +10,9 @@ export const plugins = [
   nodeResolve(),
   terser({compress: true, mangle: true}),
 ]
+
+export const bin = defineConfig({
+  plugins,
+  input: "main.ts",
+  output: {file: "main.js", format: "esm"},
+})
