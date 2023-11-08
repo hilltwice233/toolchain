cf978cb09a9d84c1c9555d94db9c1534dbe9ef16
hilltwice <hilltwice@163.com>
2023-11-09 06:52:32 +0800

    remove json comments

--- node/compress-json/index.ts
+++ node/compress-json/index.ts
@@ -1,6 +1,25 @@
+export function removeJsonComments(raw: string): string {
+  return raw.replaceAll(/(\/\/.*\n)|(\/\*(.|[\r\n])*\*\/)/g, "")
+}
+
 if (import.meta.vitest) {
   const {it, expect} = import.meta.vitest
-  it("add", () => {
-    expect(1 + 1).toEqual(2)
+  it("remove json comments", () => {
+    const raw = `{
+      // inline comment
+      "name": "hilltwice",
+      /* inline block comment */
+      /*
+       * multi-line comment
+       */
+      "age": 22,
+      "has-y-chromosome": true
+    }`
+    const resolved = removeJsonComments(raw)
+    console.log(resolved)
+    const person = JSON.parse(resolved)
+    expect(person["name"]).toBe("hilltwice")
+    expect(person["age"]).toBe(22)
+    expect(person["has-y-chromosome"]).toBe(true)
   })
 }
